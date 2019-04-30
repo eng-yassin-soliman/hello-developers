10dabd5b9f238d940523e32895580273aa74eed2
yassin-soliman <akcorp@hotmail.com>
2019-04-30 06:28:14 +0200

    ok

---
+++ s_hello_developers/p_hello_wpf/_c_app.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace p_hello_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class _c_app : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            v_start_();
+            Shutdown(0);
+        }
+
+        void v_start_()
+        {
+            MessageBox.Show("ok");
+        }
+    }
+}
