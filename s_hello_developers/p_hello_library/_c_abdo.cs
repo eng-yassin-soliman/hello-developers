7696507b3917097cb5ad20eb782a414165696c3d
ABDO <ah1326996@gmail.com>
2019-05-01 20:24:47 +0200

    لقيته بحسبها رساله :)

---
+++ s_hello_developers/p_hello_library/_c_abdo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p_hello_library
+{
+    class _c_abdo
+    {
+        static void Main(string[] args)
+
+        {
+
+       
+
+            int number = 5;
+
+            long fact = GetFactorial(number);
+
+            Console.WriteLine( fact);
+
+            Console.ReadKey();
+
+        }
+
+
+
+        private static long GetFactorial(int number)
+
+        {
+
+            if (number == 0)
+
+            {
+
+                return 1;
+
+            }
+
+            return number * GetFactorial(number - 1);
+
+        }
+
+
+
+
+    }
+}
