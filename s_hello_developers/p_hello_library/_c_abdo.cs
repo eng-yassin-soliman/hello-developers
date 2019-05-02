d088bf88ffacfaa8c04c9f27868c527402e856ec
yassin-soliman <akcorp@hotmail.com>
2019-05-02 03:59:06 +0200

    ok

--- s_hello_developers/p_hello_library/_c_abdo.cs
+++ s_hello_developers/p_hello_library/_c_abdo.cs
@@ -2,22 +2,42 @@
 using System.Collections.Generic;
 using System.Text;
 
-class _c_abdo
+public class _c_abdo
 {
+    // الدالة اللي انت كاتبها
     public static int f_factorial_(int p_num_)
     {
+        int number = 5; // ايه لازمتها؟ عندك باراميتر فوق
 
-            int number = 5;
-        long fact =1;
-        for (int i = 0; i < 5; i++)
+        long fact = 1; // الدالة بترجع انتجر مش لونج
+
+        // فين الريكورجن؟
+        for (int i = 0; i < 5; i++) // ليه حاطط الخمسة هنا؟
         {
             fact *= number;
             number--;
         }
 
-            Console.WriteLine(fact);
+        Console.WriteLine(fact); // دي كلاس ليبراري عشان نستخدمها من جوة برنامج، مش برنامج
 
-            return DateTime.Now.Second;
+        return DateTime.Now.Second; // محذفتهاش ليه؟
+    }
 
+    // الدالة بعد التعديل
+    public static int f_factorial_test_(int p_num_)
+    {
+        int number = p_num_; // 5;
+
+        long fact = 1;
+        for (int i = 0; i < p_num_; i++)
+        {
+            fact *= number;
+            number--;
+        }
+
+        return (int)fact;
+
+        // Console.WriteLine(fact);
+        // return DateTime.Now.Second;
     }
 }
