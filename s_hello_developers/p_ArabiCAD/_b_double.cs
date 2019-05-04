67dd5bc2f99b2d5a62c28d3ee85d80a2fa8f648b
yassin-soliman <akcorp@hotmail.com>
2019-05-04 02:05:44 +0200

    ok

---
+++ s_hello_developers/p_ArabiCAD/_b_double.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace p_ArabiCAD
+{
+    class _b_double : TextBox
+    {
+        public bool p_vld_ = true;
+
+        public _b_double()
+        {
+            Width = 80; Text = "0"; TextAlignment = TextAlignment.Right;
+            Margin = new Thickness(5); Padding = new Thickness(5);
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs p_arg_)
+        {
+            base.OnTextChanged(p_arg_);
+
+            try
+            {
+                double.Parse(Text);
+                p_vld_ = true; Background = new SolidColorBrush(Colors.White);
+            }
+            catch
+            {
+                p_vld_ = false; Background = new SolidColorBrush(Colors.LightPink);
+            }
+        }
+
+        public double s_value_
+        {
+            get
+            {
+                double.TryParse(Text, out double p_dbl_);
+                return p_dbl_;
+            }
+            set
+            {
+                Text = value.ToString("0.0000");
+            }
+        }
+    }
+}
