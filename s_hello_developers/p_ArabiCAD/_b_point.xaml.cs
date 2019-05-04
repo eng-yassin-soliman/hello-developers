662b332d276e886f39a80216701ea0b16c35e152
yassin-soliman <akcorp@hotmail.com>
2019-05-04 02:05:44 +0200

    ok

---
+++ s_hello_developers/p_ArabiCAD/_b_point.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace p_ArabiCAD
+{
+    /// <summary>
+    /// Interaction logic for _b_point.xaml
+    /// </summary>
+    public partial class _b_point : UserControl
+    {
+        public bool p_vld_ = true; 
+
+        public _b_point()
+        {
+            InitializeComponent();
+            b_xcr_.TextChanged += new TextChangedEventHandler(v_update_);
+        }
+
+        public void v_show_()
+        {
+
+        }
+
+        void v_update_(object p_snd_, TextChangedEventArgs p_arg_)
+        {
+            string l_str_ = ((TextBox)p_snd_).Text;
+
+        }
+
+        public Point s_point_
+        {
+            get
+            {
+                double.TryParse(b_xcr_.Text, out double l_xcr_);
+                double.TryParse(b_ycr_.Text, out double l_ycr_);
+                return new Point(l_xcr_, l_ycr_);
+            }
+
+            set
+            {
+                b_xcr_.Text = value.X.ToString();
+                b_ycr_.Text = value.Y.ToString();
+            }
+        }
+    }
+}
--- s_hello_developers/p_ArabiCAD/_w_main.xaml.cs
+++ s_hello_developers/p_ArabiCAD/_w_main.xaml.cs
@@ -17,11 +17,40 @@ namespace p_ArabiCAD
     /// <summary>
     /// Interaction logic for _w_main.xaml
     /// </summary>
+    /// 
+
     public partial class _w_main : Window
     {
+        public _b_point s_pos_ = new _b_point();
+
         public _w_main()
         {
             InitializeComponent();
+
+            b_cnv_.MouseUp += new MouseButtonEventHandler(v_click_);
+            b_cnv_.MouseMove += new MouseEventHandler(v_move_);
+        }
+
+        void v_add_()
+        {
+
+        }
+
+        void v_remove_()
+        {
+
+        }
+
+        void v_move_(object p_snd_, MouseEventArgs p_arg_)
+        {
+            Point l_pnt_ = p_arg_.GetPosition(b_cnv_);
+            b_pnt_.b_xcr_.s_value_ = l_pnt_.X;
+            b_pnt_.b_ycr_.s_value_ = l_pnt_.Y;
+        }
+
+        void v_click_(object p_snd_, MouseButtonEventArgs p_arg_)
+        {
+
         }
     }
 }
