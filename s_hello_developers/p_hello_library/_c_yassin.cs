6cc758cdba888b6510c1d8c26af0117dc8bd3d23
yassin-soliman <akcorp@hotmail.com>
2019-05-14 14:51:37 +0200

    ok

--- s_hello_developers/p_hello_library/_c_yassin.cs
+++ s_hello_developers/p_hello_library/_c_yassin.cs
@@ -13,9 +13,9 @@ public class _c_yassin
     public static int f_factorial_(int p_num_)
     {
         if (p_num_ < 2) { return 1; }
-
+ 
         return p_num_ * f_factorial_(p_num_ - 1);
         
-        // 14/5/2019 14:34
+        // 14/5/2019 6:04
     }
 }
