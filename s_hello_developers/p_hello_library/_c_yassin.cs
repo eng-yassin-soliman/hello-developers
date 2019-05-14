8e70f4c8797cd0a8fb05ff64ae47dd8271ee8f67
yassin-soliman <akcorp@hotmail.com>
2019-05-14 14:34:21 +0200

    ok

--- s_hello_developers/p_hello_library/_c_yassin.cs
+++ s_hello_developers/p_hello_library/_c_yassin.cs
@@ -6,7 +6,7 @@
 // مطلوب فنكشن بتحسب الفاكتوريال بطريقة ريكروسيف
 // recursive factorial function.
 
-public class _c_yassin 
+public class _c_yassin
 {
     public _c_yassin() { }
 
@@ -16,6 +16,6 @@ public class _c_yassin
 
         return p_num_ * f_factorial_(p_num_ - 1);
         
-        // 14/5/2019 6:04
+        // 14/5/2019 14:34
     }
 }
