b10fc0b0793272dab9f9cb3472cf9cb708d65fa6
yassin-soliman <akcorp@hotmail.com>
2019-05-20 03:11:11 +0200

    ok

--- s_hello_developers/p_ArabiCAD/segments/_c_segment.cs
+++ s_hello_developers/p_ArabiCAD/segments/_c_segment.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace p_ArabiCAD.segments 
 {
-    public class _c_segment
+    public class _c_segment 
     {
         public List<_c_constraint> s_cns_ = new List<_c_constraint>();
 
