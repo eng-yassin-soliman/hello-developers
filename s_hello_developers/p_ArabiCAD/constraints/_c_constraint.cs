bbe72b4512d7c43acffcb115e1e6a4e2488ba17e
yassin-soliman <akcorp@hotmail.com>
2019-07-01 02:26:07 +0200

    Project reference

--- s_hello_developers/p_ArabiCAD/constraints/_c_constraint.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace p_ArabiCAD.constraints
-{
-    public class _c_constraint
-    {
-        public List<double> s_val_ = new List<double>();
-    }
-}
---
+++ s_hello_developers/p_hello_library/cad/constraints/_c_constraint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace p_ArabiCAD.constraints
+{
+    /// <summary>
+    /// Base class for constraints
+    /// </summary>
+    public abstract class _c_constraint
+    {
+        /// <summary>Constraints' values</summary>
+        internal List<double> s_val_ = new List<double>();
+    }
+}
