060c3f00184ea8c429d80a5815153ea389e38d9e
ahmedahmed3000 <robotrock9901@gmail.com>
2019-07-04 01:16:02 +0200

    الهبيدة

---
+++ s_hello_developers/pr_calculator_two_numbers/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace pr_calculator_two_numbers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
