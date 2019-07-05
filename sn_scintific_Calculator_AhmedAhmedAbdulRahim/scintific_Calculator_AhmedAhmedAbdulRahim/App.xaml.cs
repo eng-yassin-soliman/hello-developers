805dcccba928feb563cb492b7b095395a666f2f1
ahmedahmed3000 <robotrock9901@gmail.com>
2019-07-05 09:35:28 +0200

    التاسك الفردى لأحمد عبدالرحيم using mouse only

---
+++ sn_scintific_Calculator_AhmedAhmedAbdulRahim/scintific_Calculator_AhmedAhmedAbdulRahim/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace scintific_Calculator_AhmedAhmedAbdulRahim
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
