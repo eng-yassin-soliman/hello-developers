d7181e804c68d5eba32e0f7d4f27f913666f2c55
Adnan <Adnan@DESKTOP-9KT88SG>
2019-07-16 15:09:02 +0200

    d

---
+++ s_hello_developers/P_hello_api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P_hello_api.Controllers
+{
+    [Route("hello/api")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<string> s_lst_ = new List<string>();
+        [HttpGet]
+        public string f_date_()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+ }
