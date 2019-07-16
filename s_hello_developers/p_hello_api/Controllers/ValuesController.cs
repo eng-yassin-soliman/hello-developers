c18d14a3ffb0f0c404fb9365fff2412dca065fd7
yassin-soliman <akcorp@hotmail.com>
2019-07-16 11:02:24 +0200

    ok

--- s_hello_developers/p_hello_api/Controllers/ValuesController.cs
+++ s_hello_developers/p_hello_api/Controllers/ValuesController.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using
+using System.Text;
+using System.Net;
 
-    namespace p_hello_api.Controllers
+namespace p_hello_api.Controllers
 {
+    public class _c_score
+    {
+
+    }
+
     [Route("hello/api")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -16,9 +22,28 @@ using
         [HttpPost]
         public string f_get_()
         {
-            foreach (string i_str_ in s_lst_)
-            {                
-                string[] l_str_ = i_str_.split("-");
+            //System.Net.IPAddress.
+              //  ;
+            //.MapToIPv4.ToString();
+
+            for (int i_str_ = 0; i_str_ < s_lst_.Count; i_str_++)
+            {
+
+
+                System.IO.StreamReader l_srd_ = new System.IO.StreamReader(HttpContext.Request.Body,Encoding.UTF8);
+
+                string l_txt_ = l_srd_.ReadToEnd();
+                string[] l_str_ = s_lst_[i_str_].Split("-");
+                string[] l_req_ = l_txt_.Split("-");
+
+                if (l_str_[0] == l_req_[0])
+                {
+                    s_lst_[i_str_] = l_txt_;
+                }
+                else
+                {
+                    s_lst_.Add(l_txt_);
+                };
             }
 
             return "ok";
