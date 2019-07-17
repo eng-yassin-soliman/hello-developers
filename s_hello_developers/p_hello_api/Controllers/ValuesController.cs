381ff6710c935c3c7e5043e13451d83a4309949e
1808714@eng.asu.edu.eg <1808714@eng.asu.edu.eg>
2019-07-17 08:31:59 +0200

    Hello API

--- s_hello_developers/p_hello_api/Controllers/ValuesController.cs
+++ s_hello_developers/p_hello_api/Controllers/ValuesController.cs
@@ -1,63 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Text;
+using System.Net;
 namespace p_hello_api.Controllers
 {
     [Route("hello/api")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        //// GET api/values
-        //[HttpGet]
-        //public string f_get_()
-        //{
-        //    return "hello";
-        //}
-
-
-
-        //byte l_byte = byte.TryParse(l_txt_, out p_num_1);
-        // POST api/values
-        [HttpPost]
-        public string f_post_()
+        public static List<string> s_lst_ = new List<string>();
+        [HttpGet]
+        public string f_date_()
         {
-            System.IO.StreamReader l_srd_ = new System.IO.StreamReader(HttpContext.Request.Body, Encoding.UTF8);
-            /*
-            string l_txt_ = l_srd_.ReadToEnd();
-
-            byte[] l_byt_ = Encoding.UTF8.GetBytes(l_txt_);
-
-    */
-
-            string l_txt_ = l_srd_.ReadToEnd();
-            byte byteValue;
-            bool success = Byte.TryParse(l_txt_, out byteValue);
-            string l_string = Encoding.UTF8.GetString(byteValue);
-
-
-
-
-
-
-
-
-
-
-
-
-            StringBuilder l_str_ = new StringBuilder("");
-
-            foreach (byte i_byt_ in l_byt_)
-            {
-                l_str_.Append(i_byt_.ToString());
-                l_str_.Append("--");
-            }
-
-            return l_str_.ToString();
+            return DateTime.Now.ToString();
         }
     }
 }
