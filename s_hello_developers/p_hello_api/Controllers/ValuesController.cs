1e417432dd5002b5b1a99f07b991b2d6390b855a
ahmedahmed3000 <robotrock9901@gmail.com>
2019-07-18 05:41:06 +0200

    task1

--- s_hello_developers/p_hello_api/Controllers/ValuesController.cs
+++ s_hello_developers/p_hello_api/Controllers/ValuesController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Net;
+using System.IO;
+
 namespace p_hello_api.Controllers
 {
     [Route("hello/api")]
@@ -16,5 +18,19 @@ namespace p_hello_api.Controllers
         {
             return DateTime.Now.ToString();
         }
+
+        // Previous method
+        /// <summary>A simple method that returns the same request body!!</summary>
+        /// <returns>String with the contents of request body</returns>
+        [HttpPost]
+        public string f_post_()
+        {
+            StreamReader l_red_ = new StreamReader(HttpContext.Request.Body, Encoding.UTF8);
+            string l_txt_ = l_red_.ReadToEnd();
+            return l_txt_;
+        }
     }
 }
+
+
+    
