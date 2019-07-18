f6a82eb9a017fe48f912c05e995e3cc725c09ae0
ahmedahmed3000 <robotrock9901@gmail.com>
2019-07-18 05:12:47 +0200

    t1,p1

---
+++ s_hello_developers/p_hello_api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Net;
+namespace p_hello_api.Controllers
+{
+    [Route("hello/api")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public string f_date_()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
