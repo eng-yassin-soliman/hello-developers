544d96aa017d587e32202aa65c221dcb75fc59cd
yassin-soliman <akcorp@hotmail.com>
2019-07-21 14:36:37 +0200

    ok

--- s_hello_developers/p_hello_api/Controllers/CADController.cs
+++ s_hello_developers/p_hello_api/Controllers/CADController.cs
@@ -6,32 +6,47 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Net;
 using System.IO;
+using Remotion.Linq.Clauses.ExpressionVisitors;
 
 namespace p_hello_api.Controllers
 {
-    class _c_drawing
+    public class _c_drawing
     {
-        public int s_uid_ = 0;
-        public string s_nam_ = string.Empty;
-        public string s_dat_ = string.Empty;
+        public int s_uid_ = 0;                  // ID
+        public string s_nam_ = string.Empty;    // Name
+        public string s_dat_ = string.Empty;    // Drawing data
+        public string s_lin_ = string.Empty;    // Line color
+        public string s_fil_ = string.Empty;    // Filling color of the shape
     }
 
     [Route("CAD/api")]
     [ApiController]
     public class CADController : Controller
     {
-        List<_c_drawing> s_lst_ = new List<_c_drawing>();
+        static List<_c_drawing> s_lst_ = new List<_c_drawing>();
 
         /// <summary>A method that returns the drawing data for all of the team</summary>
         /// <param name="p_drw_">The drawing data of the client</param>
         /// <returns>JSON formatted array of _c_drawing</returns>
         [HttpPost]
-        public string f_post_(_c_drawing p_drw_)
+        public _c_drawing[] f_post_(_c_drawing p_drw_)
         {
-            StreamReader l_red_ = new StreamReader(HttpContext.Request.Body, Encoding.UTF8);
+            // check if uid exists in the array
+            var l_qur_ = (from q_drw_ in s_lst_
+                          where q_drw_.s_uid_ == p_drw_.s_uid_
+                          select q_drw_);
 
-            string l_txt_ = l_red_.ReadToEnd();
-            return l_txt_;
+            if (!l_qur_.Any())
+            {
+                s_lst_.Add(p_drw_);
+            }
+            else
+            {
+                l_qur_.FirstOrDefault().s_nam_ = p_drw_.s_nam_;
+                l_qur_.FirstOrDefault().s_dat_ = p_drw_.s_dat_;
+            }
+
+            return s_lst_.ToArray();
         }
     }
 }
--- s_hello_developers/p_hello_api/wwwroot/_j_cad.js
+++ s_hello_developers/p_hello_api/wwwroot/_j_cad.js
@@ -1,12 +1,12 @@
 ﻿// A JavaScript method to make http requests
 // p_scc_: A function to call on success
 // p_err_: A function to call on error
-function v_post_(p_msg_, p_scc_, p_err_)
+function v_post_(p_url_, p_msg_, p_typ_, p_scc_, p_err_)
 {
     fetch(
-        'CAD/api',        // URL to post to
+        'CAD/api',          // URL to post to
         {   // Array of HTTP request headers
-            headers: { "Content-Type": "text/plain; charset=utf-8"},
+            headers: { "Content-Type" : p_typ_ },
             method: 'POST', // HTTP verb: POST, GET, etc..
             body: p_msg_,   // HTTP request body
         })
@@ -15,10 +15,15 @@ function v_post_(p_msg_, p_scc_, p_err_)
         .catch(function (p_rsp_) { p_err_(p_rsp_); });
 }
 
+function v_post_txt_(p_url_, p_msg_, p_scc_, p_err_)
+{
+    v_post_(p_url_, p_msg_, "text/plain; charset=UTF-8", p_scc_, p_err_)
+}
+
 // Convert objects (instances of classes) to string (encoded by JSON format)
 // Then post them as text
-function v_post_obj_(p_msg_, p_scc_, p_err_)
+function v_post_obj_(p_url_, p_msg_, p_scc_, p_err_)
 {
     var l_msg_ = JSON.stringify(p_msg_);
-    v_post_(l_msg_, p_scc_, p_err_)
+    v_post_(p_url_, l_msg_, "application/json; charset=UTF-8", p_scc_, p_err_)
 }
