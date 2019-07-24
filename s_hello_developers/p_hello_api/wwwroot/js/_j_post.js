766886fd2187e3f351801bca5e8c35afd07ae1c6
yassin-soliman <akcorp@hotmail.com>
2019-07-24 12:04:46 +0200

    ok

--- s_hello_developers/p_hello_api/wwwroot/js/_j_post.js
+++ s_hello_developers/p_hello_api/wwwroot/js/_j_post.js
@@ -1,14 +1,11 @@
-﻿// A JavaScript method to make http requests
-// p_scc_: A function to call on success
-// p_err_: A function to call on error
-function v_post_(p_url_, p_msg_, p_typ_, p_scc_, p_err_)
+﻿function v_post_(p_url_, p_msg_, p_typ_, p_scc_, p_err_)
 {
     fetch(
-        'CAD/api',          // URL to post to
-        {   // Array of HTTP request headers
+        p_url_,
+        {
             headers: { "Content-Type" : p_typ_ },
-            method: 'POST', // HTTP verb: POST, GET, etc..
-            body: p_msg_,   // HTTP request body
+            method: 'POST',
+            body: p_msg_,
         })
         .then(function (p_rsp_) { return p_rsp_.text(); })
         .then(function (p_txt_) { p_scc_(p_txt_); })
@@ -20,8 +17,6 @@ function v_post_txt_(p_url_, p_msg_, p_scc_, p_err_)
     v_post_(p_url_, p_msg_, "text/plain; charset=UTF-8", p_scc_, p_err_)
 }
 
-// Convert objects (instances of classes) to string (encoded by JSON format)
-// Then post them as text
 function v_post_obj_(p_url_, p_msg_, p_scc_, p_err_)
 {
     var l_msg_ = JSON.stringify(p_msg_);
