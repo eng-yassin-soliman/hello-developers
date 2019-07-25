046e6f2400e00642d6c13ebc088759db6eae1468
yassin-soliman <akcorp@hotmail.com>
2019-07-25 16:54:54 +0200

    ok

---
+++ s_hello_developers/p_hello_api/Controllers/_c_hello_api.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace p_hello_api
+{
+    [Route("hello/api")]
+    [ApiController]
+    public class _c_hello_api : Controller
+    {
+        [HttpGet]
+        public string f_date_()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
---
+++ s_hello_developers/p_hello_api/wwwroot/js/_j_post.js
@@ -0,0 +1,25 @@
+﻿function v_post_(p_url_, p_msg_, p_typ_, p_scc_, p_err_)
+{
+    fetch(
+        p_url_,
+        {
+            headers: { "Content-Type" : p_typ_ },
+            method: 'POST',
+            body: p_msg_,
+        })
+        .then(function (p_rsp_) { return p_rsp_.text(); })
+        .then(function (p_txt_) { p_scc_(p_txt_); })
+        .catch(function (p_rsp_) { p_err_(p_rsp_); });
+}
+
+function v_post_txt_(p_url_, p_msg_, p_scc_, p_err_)
+{
+    v_post_(p_url_, p_msg_, "text/plain; charset=UTF-8", p_scc_, p_err_)
+}
+
+function v_post_obj_(p_url_, p_msg_, p_scc_, p_err_)
+{
+    var l_msg_ = JSON.stringify(p_msg_);
+
+    v_post_(p_url_, l_msg_, "application/json; charset=UTF-8", p_scc_, p_err_)
+}
