8c12d60de6851aceae8fb3b0150001696698e1ba
ahmedahmed3000 <robotrock9901@gmail.com>
2019-08-01 05:25:00 +0200

    TaskWeb

--- s_hello_developers/p_hello_api/Controllers/_c_hello_api.cs
+++ s_hello_developers/p_hello_api/Controllers/_c_hello_api.cs
@@ -13,5 +13,28 @@ namespace p_hello_api
         {
             return DateTime.Now.ToString();
         }
+        [HttpPost]
+        [Route("text")]
+        public string f_text_()
+        {
+            StreamReader l_red_ = new StreamReader(HttpContext.Request.Body, Encoding.UTF8);
+            string l_txt_ = l_red_.ReadToEnd();
+            return l_txt_;
+        }
+        [HttpPost]
+        [Route("byte")]
+        public string f_byte_()
+        {
+            StreamReader l_red_ = new StreamReader(HttpContext.Request.Body, Encoding.UTF8);
+            string l_txt_ = l_red_.ReadToEnd();
+            byte[] l_byt_ = Encoding.UTF8.GetBytes(l_txt_);
+            StringBuilder l_str_ = new StringBuilder(string.Empty);
+            foreach (byte i_byt_ in l_byt_)
+            {
+                l_str_.Append(i_byt_.ToString("000"));
+                l_str_.Append(Encoding.UTF8.GetString(new byte[] { 10 })); // New line
+            }
+            return l_str_.ToString();
+        }
     }
 }
