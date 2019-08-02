fedf398318e6acf42e79bb00e532ec71bfdd5353
yassin-soliman <akcorp@hotmail.com>
2019-08-02 23:07:03 +0200

    ok

--- s_hello_developers/p_hello_api/DAL/_c_dal.cs
+++ s_hello_developers/p_hello_api/DAL/_c_dal.cs
@@ -6,25 +6,34 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+//using 
 
 namespace p_hello_api.DAL
 {
     public class _c_dal
     {
+        public MySqlConnection s_con_;
+        public MySqlTransaction s_tra_;
+        public _c_ctx s_ctx_;
+
         Boolean f_connect_()
-        {            
+        {
+            s_con_ = new MySqlConnection(
             using (MySqlConnection l_con_ = new MySqlConnection(""))
             {
+                l_con_.Open();
+
                 using (MySqlTransaction l_tra_ = l_con_.BeginTransaction(System.Data.IsolationLevel.Serializable))
                 {
                     _c_ctx l_ctx_ = new _c_ctx();
-                    
                 }
             }
 
 
             return true;
         }
+
+        Boolean f_close(Boolean p_cmt_) { }
     }
 
     public class _t_member
