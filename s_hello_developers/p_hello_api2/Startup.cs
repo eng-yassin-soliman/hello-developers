d17f081fc92e284fef60c7f8a037010bc7d588c7
ahmed <ahmedabdo9864@gmail.com>
2019-08-15 17:10:35 +0200

    first and second task

---
+++ s_hello_developers/p_hello_api2/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace p_hello_api2
+{
+    public class Startup
+    {
+        public Startup
+        (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration
+        {
+            get;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if
+             (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseFileServer();
+            app.UseMvc();
+        }
+    }
+}
---
+++ s_hello_developers/p_hello_api3/Controllers/_c_hello_api3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+namespace p_hello_api3
+{
+    [Route("hello/api")]
+    [ApiController]
+    public class _c_hello_api3 : Controller
+    {
+        [HttpGet]
+        public string f_date_()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
+
+
