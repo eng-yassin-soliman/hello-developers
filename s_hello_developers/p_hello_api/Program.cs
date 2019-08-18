ea4d6725c9a7574141896e928b7d558fede6726c
yassin-soliman <akcorp@hotmail.com>
2019-08-18 10:50:23 +0200

    ok

--- s_hello_developers/p_hello_api/Program.cs
+++ s_hello_developers/p_hello_api/Program.cs
@@ -7,9 +7,34 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace p_hello_api
 {
+    class ffff
+    {
+        [JsonConverter(typeof(ByteArrayConvertor))]
+        public byte[] l_byt_ = { 0, 1, 2, 3 };
+    }
+
+    public class ByteArrayConvertor : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(byte[]);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue("[0, 0, 0]");
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
