4f6a7b6159a01b23ce10fdbb0d1800944352bbdf
yassin-soliman <akcorp@hotmail.com>
2019-08-31 22:20:06 +0200

    ok

---
+++ s_hello_developers/p_hello_xamarin/p_hello_xamarin/_w_main.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace p_hello_xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class _w_main : TabbedPage
+    {
+        public _w_main()
+        {
+            InitializeComponent();
+        }
+    }
+}
