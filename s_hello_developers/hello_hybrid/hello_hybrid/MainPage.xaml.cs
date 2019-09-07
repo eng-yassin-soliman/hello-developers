9736a656718e8d47ebbcaefa043c6a23504c2ddf
yassin-soliman <akcorp@hotmail.com>
2019-09-07 08:24:39 +0200

    ok

--- s_hello_developers/hello_hybrid/hello_hybrid/MainPage.xaml.cs
+++ s_hello_developers/hello_hybrid/hello_hybrid/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 using Xamarin.Forms;
 
 namespace hello_hybrid
-{
+{ 
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
