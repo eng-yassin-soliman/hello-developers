c0b6b9bfdae4f008bc30163d704a2e378dd06e42
yassin-soliman <akcorp@hotmail.com>
2019-09-16 10:03:30 +0200

    ok

---
+++ s_hello_developers/p_hello_xamarin/p_hello_xamarin/App.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using p_hello_xamarin.Services;
+using p_hello_xamarin.ViewModels;
+
+namespace p_hello_xamarin
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<_i_helloserv, _c_helloserv>();
+
+            RegisterAppStart<_c_mainview>();
+        }
+    }
+}
---
+++ s_hello_developers/p_hello_xamarin/p_hello_xamarin/Services/_c_helloserv.cs
@@ -0,0 +1,10 @@
+﻿namespace p_hello_xamarin.Services
+{
+    public class _c_helloserv : _i_helloserv
+    {
+        public double f_tip_(double subTotal, int generosity)
+        {
+            return subTotal * generosity / 100.0;
+        }
+    }
+}
