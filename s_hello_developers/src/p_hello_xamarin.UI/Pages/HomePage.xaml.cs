54d9056951a463b551b45e68a5fd4184d18f195c
yassin-soliman <akcorp@hotmail.com>
2019-09-19 04:26:42 +0200

    ok

--- s_hello_developers/src/p_hello_xamarin.UI/Pages/HomePage.xaml.cs
+++ s_hello_developers/src/p_hello_xamarin.UI/Pages/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ using Xamarin.Forms.Xaml;
 
 namespace p_hello_xamarin.UI.Pages
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Skip)]
     [MvxContentPagePresentation(WrapInNavigationPage = true)]
     public partial class HomePage : MvxContentPage<HomeViewModel>
     {
