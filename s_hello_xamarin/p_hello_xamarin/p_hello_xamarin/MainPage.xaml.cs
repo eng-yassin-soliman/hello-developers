ddee5d6f6ff28ff71e02a30e1555dfda7b8ddf75
yassin-soliman <yassin_soliman@outlook.com>
2020-07-10 21:35:09 +0200

    2020-07-10 09:34 PM GMT+2

--- s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
+++ s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
@@ -5,22 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using org.arbweb.xam.ui;
 
 namespace p_hello_xamarin
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(true)]
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
-
-        void v_clicked_(object sender, EventArgs e)
+        
+        protected override void OnAppearing()
         {
+            v_permit_async_();
+        }
 
+        async Task v_permit_async_()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationAlways>();
+            }
+
+            var b_web_ = new _c_hybridwebview { Uri = "home.html" };
+            Content = b_web_;
+            b_web_.HorizontalOptions = LayoutOptions.Fill;
+            b_web_.VerticalOptions = LayoutOptions.Fill;
+
+            //b_web_.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
         }
     }
 }
