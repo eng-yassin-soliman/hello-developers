ff99566a8f4e8706daf06067b27dd738db5243c3
yassin-soliman <yassin_soliman@outlook.com>
2020-07-10 10:10:45 +0200

    2020-07-10 10:10 AM GMT+2

--- s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
+++ s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
@@ -10,7 +10,7 @@ namespace p_hello_xamarin
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
@@ -20,20 +20,7 @@ namespace p_hello_xamarin
 
         void v_clicked_(object sender, EventArgs e)
         {
-            try
-            {
-                Double l_1st_ = Double.Parse(b_1st_.Text);
-                Double l_2nd_ = Double.Parse(b_2nd_.Text);
 
-                Double l_res_ = l_1st_ * l_2nd_;
-
-                b_res_.Text = l_res_.ToString();
-            }
-            catch
-            {
-                b_res_.BackgroundColor = Color.LightYellow;
-                b_res_.Text = "خطأ في المدخلات";
-            }
         }
     }
 }
