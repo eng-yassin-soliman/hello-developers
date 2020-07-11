7ec1c92717b7c83526032e86a84def23915a0a81
yassin-soliman <yassin_soliman@outlook.com>
2020-07-11 11:05:12 +0200

    Added native deolocation

--- s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
+++ s_hello_xamarin/p_hello_xamarin/p_hello_xamarin/MainPage.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Threading.Tasks;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
 using Xamarin.Essentials;
-using org.arbweb.xam.ui;
+// using org.arbweb.xam.ui;
+using Xamarin.Forms;
+using Plugin.HybridWebView.Shared;
+using Plugin.HybridWebView.Shared.Enumerations;
 
 namespace p_hello_xamarin
 {
@@ -31,12 +29,8 @@ namespace p_hello_xamarin
                 status = await Permissions.RequestAsync<Permissions.LocationAlways>();
             }
 
-            var b_web_ = new _c_hybridwebview { Uri = "home.html" };
-            Content = b_web_;
-            b_web_.HorizontalOptions = LayoutOptions.Fill;
-            b_web_.VerticalOptions = LayoutOptions.Fill;
-
-            //b_web_.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            b_acc_.Text = location.Accuracy.ToString();
         }
     }
 }
