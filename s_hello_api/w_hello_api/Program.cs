99dd5e6a419abf36e1cf4cf3ceca30e6796418bc
yassin-soliman <yassin_soliman@outlook.com>
2020-07-25 19:40:48 +0200

    HTTP post method with single parameter

--- s_hello_api/w_hello_api/Program.cs
+++ s_hello_api/w_hello_api/Program.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace w_hello_api
--- s_hello_api/w_hello_api/wwwroot/js/_j_post.js
+++ s_hello_api/w_hello_api/wwwroot/js/_j_post.js
@@ -17,11 +17,13 @@
     f_req_();
 }
 
-function v_post_txt_(p_url_, p_msg_, p_scc_, p_err_) {
+function v_post_txt_(p_url_, p_msg_, p_scc_, p_err_)
+{
     v_post_(p_url_, p_msg_, "text/plain; charset=UTF-8", p_scc_, p_err_)
 }
 
-function v_post_obj_(p_url_, p_msg_, p_scc_, p_err_) {
+function v_post_obj_(p_url_, p_msg_, p_scc_, p_err_)
+{
     var l_msg_ = JSON.stringify(p_msg_);
 
     v_post_(p_url_, l_msg_, "application/json; charset=UTF-8", p_scc_, p_err_)
