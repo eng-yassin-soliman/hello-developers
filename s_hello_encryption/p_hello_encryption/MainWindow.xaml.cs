9baa6109a2b56d8b4d5c0d25888131d77788ecc0
yassin-soliman <yassin_soliman@outlook.com>
2020-09-07 14:23:25 +0200

    Added asymmetric algorithm to s_encryption

--- s_hello_encryption/p_hello_encryption/MainWindow.xaml.cs
+++ s_hello_encryption/p_hello_encryption/MainWindow.xaml.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Security.Cryptography;
+using System.IO;
 using System.Text;
+using System.Linq;
 using System.Windows;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace p_hello_encryption
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         // Symmetric encryption
@@ -20,19 +18,30 @@ namespace p_hello_encryption
         public byte[] s_pad_ = Encoding.UTF8.GetBytes("9F4834KE");
 
         TripleDESCryptoServiceProvider s_tds_;
+        RSA s_rsa_ = RSA.Create();
 
         public MainWindow()
         {
             InitializeComponent();
 
+            // Symmetric algorithm
             s_tds_ = new TripleDESCryptoServiceProvider();
             s_tds_.Key = s_key_.Take(16).ToArray(); // A 16 bytes Key
             s_tds_.IV = s_key_.Skip(16).ToArray();  // An 8 bytes IV
 
             s_tds_.Mode = CipherMode.ECB;
             s_tds_.Padding = PaddingMode.PKCS7;
+
+            // Asymmetric algorithm
+            string l_dir_ = "D:\\Github\\eng-yassin-soliman\\hello-developers\\s_hello_encryption\\p_hello_encryption\\rsa\\";
+            byte[] l_pub_ = Convert.FromBase64String(File.ReadAllText(l_dir_ + "pub.txt").Replace("/r/n",""));
+            byte[] l_pri_ = Convert.FromBase64String(File.ReadAllText(l_dir_ + "pri.txt").Replace("/r/n", ""));
+
+            s_rsa_.ImportSubjectPublicKeyInfo(l_pub_, out _);
+            s_rsa_.ImportPkcs8PrivateKey(l_pri_, out _);
         }
 
+        #region "Symmetric"
         void v_symmetric_enc_(object p_snd_, RoutedEventArgs p_arg_)
         {
             ICryptoTransform l_enc_ = s_tds_.CreateEncryptor(); // Notice: (En-cryptor!)
@@ -66,7 +75,28 @@ namespace p_hello_encryption
 
             b_inp_.Text = Encoding.UTF8.GetString(l_out_.Skip(8).ToArray()); // Remove padding
         }
+        #endregion
 
+        #region "Asymmetric"
+        void v_asymmetric_enc_(object p_snd_, RoutedEventArgs p_arg_)
+        {
+            byte[] l_inp_ = Encoding.UTF8.GetBytes(b_inp_.Text);
+            byte[] l_out_ = s_rsa_.Encrypt(l_inp_, RSAEncryptionPadding.OaepSHA256);
+            b_out_.Text = BitConverter.ToString(l_out_);
+        }
+
+        void v_asymmetric_dec_(object p_snd_, RoutedEventArgs p_arg_)
+        {
+            string[] l_hxs_ = b_out_.Text.Split("-");
+            byte[] l_cph_ = (from i_hex_ in l_hxs_
+                             select byte.Parse(i_hex_, NumberStyles.HexNumber)).ToArray();
+
+            byte[] l_out_ = s_rsa_.Decrypt(l_cph_, RSAEncryptionPadding.OaepSHA256);
+            b_inp_.Text = Encoding.UTF8.GetString(l_out_);
+        }
+        #endregion
+
+        #region "Hashing"
         byte[] f_hash_() 
         {
             SHA256 l_hsh_ = SHA256.Create();
@@ -79,7 +109,7 @@ namespace p_hello_encryption
             b_out_.Text = BitConverter.ToString(l_out_);
         }
 
-        void v_check_(object p_snd_, RoutedEventArgs p_arg_) 
+        void v_verify_(object p_snd_, RoutedEventArgs p_arg_) 
         {
             byte[] l_out_ = f_hash_();
             if (b_out_.Text != BitConverter.ToString(l_out_))
@@ -90,5 +120,6 @@ namespace p_hello_encryption
 
             MessageBox.Show("Ok");
         }
+        #endregion
     }
 }
