f84cac97dba00995dd228e6aa8def3597c5a05fd
يس سليمان <yassin_soliman@outlook.com>
2021-06-06 22:36:42 +0200

    Resolve conflicts

--- s_scratchy/p_scratchy/p_scratchy/App.xaml.cs
+++ s_scratchy/p_scratchy/p_scratchy/App.xaml.cs
@@ -10,7 +10,7 @@ namespace p_scratchy
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
